647b5de5dcf257eacbc4f6675fa427aa862ad19e
Per Näslund <per.naslund@dotnetmentor.se>
2017-12-08 08:54:45 +0100

    day 2 part 2

--- day2/app.js
+++ day2/app.js
@@ -8,10 +8,18 @@ const fsp = require('fs-extra');
     console.log('error: ', e)
   }
 
+  // part 1
+
   const data = input.split(/\n/).map(row => row.split(/\s/).map(Number).filter(Boolean))
-  
   const sum = data.reduce((acc, curr) => acc + checkSum(curr), 0)
-  console.log('sum ', sum)
+  console.log('sum1 ', sum)
+
+  // part 2
+
+  const sorted = data.map(row => row.sort((a, b) => b - a))
+  const sum2 = data.reduce((acc, curr) => acc + evenlyDivisable(curr), 0)
+
+  console.log('sum2 ', sum2)
 })();
 
 function checkSum (row) {
@@ -24,4 +32,16 @@ function checkSum (row) {
   }
 
   return biggest - smallest
+}
+
+function evenlyDivisable(row) {
+  for (let i = 0; i < row.length - 1; i++) {
+    let a = row[i]
+    for (let j = i + 1; j < row.length; j++) {
+      let b = row[j]
+      if (a % b === 0) {
+        return a / b
+      }
+    }
+  }
 }
